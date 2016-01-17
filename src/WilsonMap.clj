a055d1a504df324447079186cff923fc0bf597d6
Jia Deng <flyucarl@gmail.com>
2016-01-17 15:29:49 +0800

    add code of wilsion map

---
+++ src/WilsonMap.clj
@@ -0,0 +1,145 @@
+(ns WilsonMap)
+(defn maze
+  "Returns a random maze carved out of walls; walls is a set of
+  2-item sets #{a b} where a and b are locations.
+  The returned maze is a set of the remaining walls."
+  [walls]
+  (let [paths (reduce (fn [index [a b]]
+                        (merge-with into index {a [b] b [a]}))
+                      {} (map seq walls))
+        start-loc (rand-nth (keys paths))]
+    (loop [walls walls
+           unvisited (disj (set (keys paths)) start-loc)]
+      (if-let [loc (when-let [s (seq unvisited)] (rand-nth s))]
+        (let [walk (iterate (comp rand-nth paths) loc)
+              steps (zipmap (take-while unvisited walk) (next walk))]
+          (recur (reduce disj walls (map set steps))
+                 (reduce disj unvisited (keys steps))))
+        walls))))
+
+(loop [x 5]
+  (if (neg? x)
+    x
+    (recur (dec x))))
+
+(defn countdown
+  [x]
+  (if (zero? x)
+    :blastoff!
+    (do (println x)
+        (recur (dec x)))))
+
+(countdown 5)
+
+(defn grid
+  [w h]
+  (set (concat
+         (for [i (range (dec w)) j (range h)] #{[i j] [(inc i) j]})
+         (for [i (range w) j (range (dec h))] #{[i j] [i (inc j)]}))))
+
+(grid 2 2)
+
+(defn draw
+  [w h maze]
+  (doto (javax.swing.JFrame. "Maze")
+    (.setContentPane
+      (doto (proxy [javax.swing.JPanel] []
+              (paintComponent [^java.awt.Graphics g]
+                (let [g (doto ^java.awt.Graphics2D (.create g)
+                          (.scale 10 10)
+                          (.translate 1.5 1.5)
+                          (.setStroke (java.awt.BasicStroke. 0.4)))]
+                  (.drawRect g -1 -1 w h)
+                  (doseq [[[xa ya] [xb yb]] (map sort maze)]
+                    (let [[xc yc] (if (= xa xb)
+                                    [(dec xa) ya]
+                                    [xa (dec ya)])]
+                      (.drawLine g xa ya xc yc))))))
+        (.setPreferredSize (java.awt.Dimension.
+                             (* 10 (inc w)) (* 10 (inc h))))))
+    .pack
+    (.setVisible true)))
+
+(draw 3 3 (maze (grid 3 3)))
+(doto (new java.util.HashMap) (.put "a" 1) (.put "b" 2))
+(doto ^java.awt.Graphics2D (.create g)
+                          (.scale 10 10)
+                          (.translate 1.5 1.5)
+                          (.setStroke (java.awt.BasicStroke.0.4)))
+(java.awt.BasicStroke. 0.4)
+
+(def frame (javax.swing.JFrame. "Hello Frame"))
+(.setSize frame 200 200)
+(.setVisible frame true)
+(defn wmaze
+  "Returns a random maze carved out of walls; walls is a set of
+  2-item sets #{a b} where a and b are locations.
+  The returned maze is a set of the remaining walls."
+  [walls]
+  (let [paths (reduce (fn [index [a b]]
+                        (merge-with into index {a [b] b [a]}))
+                      {} (map seq walls))
+        start-loc (rand-nth (keys paths))]
+    (loop [walls walls
+           unvisited (disj (set (keys paths)) start-loc)]
+      (if-let [loc (when-let [s (seq unvisited)] (rand-nth s))]
+        (let [walk (iterate (comp rand-nth paths) loc)
+              steps (zipmap (take-while unvisited walk) (next walk))
+              walk (take-while identity (iterate steps loc))
+              steps (zipmap walk (next walk))]
+          (recur (reduce disj walls (map set steps))
+                 (reduce disj unvisited (keys steps))))
+        walls))))
+
+(draw 3 3 (wmaze (grid 3 3)))
+
+(defn hex-grid
+  [w h]
+  (let [verticles (set (for [y (range h) x (range (if (odd? y) 1 0) (* 2 w) 2)]
+                         [x y]))
+        deltas [[2 0] [1 1] [-1 1]]]
+    (set (for [v verticles d deltas f [+ -]
+               :let [w (verticles (map f v d))]
+               :when w] #{v w}))))
+
+(defn- hex-outer-walls
+  [w h]
+  (let [verticles (set (for [y (range h) x (range (if (odd? y) 1 0) (* 2 w) 2)]
+                         [x y]))
+        deltas [[2 0] [1 1] [-1 1]]]
+    (set (for [v verticles d deltas f [+ -]
+               :let [w (map f v d)]
+               :when (not (verticles w))] #{v (vec w)}))))
+
+(defn hex-draw
+  [w h maze]
+  (doto (javax.swing.JFrame. "Maze")
+    (.setContentPane
+      (doto (proxy [javax.swing.JPanel] []
+              (paintComponent [^java.awt.Graphics g]
+                (let [maze (into maze (hex-outer-walls w h))
+                      g (doto ^java.awt.Graphics2D (.create g)
+                          (.scale 10 10)
+                          (.translate 1.5 1.5)
+                          (.setStroke (java.awt.BasicStroke. 0.4
+                                                             java.awt.BasicStroke/CAP_ROUND
+                                                             java.awt.BasicStroke/JOIN_MITER)))
+                      draw-line (fn [[[xa ya] [xb yb]]]
+                                  (.draw g
+                                         (java.awt.geom.Line2D$Double.
+                                           xa (* 2 ya) xb (* 2 yb))))]
+                  (doseq [[[xa ya] [xb yb]] (map sort maze)]
+                    (draw-line
+                      (cond
+                        (= ya yb) [[(inc xa) (+ ya 0.4)] [(inc xa) (- ya 0.4)]]
+                        (< ya yb) [[(inc xa) (+ ya 0.4)] [xa (+ ya 0.6)]]
+                        :else [[(inc xa) (- ya 0.4)] [xa (- ya 0.6)]]))))))
+        (.setPreferredSize (java.awt.Dimension.
+                             (* 20 (inc w)) (* 20 (+ 0.5 h))))))
+    .pack
+    (.setVisible true)))
+
+
+(hex-draw 20 20 (maze (hex-grid 20 20)))
+(hex-draw 2 2 (hex-grid 2 2))
+(hex-draw 2 2 (hex-outer-walls 2 2))
