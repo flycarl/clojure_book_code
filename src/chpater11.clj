0ef36c9f109c92973a93d9b319dbc54f662a6a50
Jia Deng <flyucarl@gmail.com>
2016-03-31 23:55:50 +0800

    number operations

--- src/chpater11.clj
+++ src/chpater11.clj
@@ -47,3 +47,63 @@ k
 (inc (bigint k))
 
 (* 100 (bigdec Double/MAX_VALUE))
+
+102233726364547159000
+(* 2 102233726364547159000)
+
+(inc' 1)
+(inc' (dec' Long/MAX_VALUE))
+
+(unchecked-dec Long/MIN_VALUE)
+(unchecked-multiply 92233720368547758 1000)
+
+(inc Long/MAX_VALUE)
+(set! *unchecked-math* true)
+(inc Long/MAX_VALUE)
+(set! *unchecked-math* false)
+
+(binding [*unchecked-math* true]
+  (inc Long/MAX_VALUE))
+(/ 22M 7)
+
+(with-precision 10 (/ 22M 7))
+(with-precision 10 :rounding FLOOR (/ 22M 7))
+
+(set! *math-context* (java.math.MathContext. 10 java.math.RoundingMode/FLOOR))
+(/ 22M 7)
+(/ 355M 113)
+
+(identical? "foot" (str "fo" "ot"))
+(let [a (range 10)]
+  (identical? a a))
+
+(identical? 5/4 (+ 3/4 1/2))
+(identical? 5.4321 5.4321)
+(identical? 2600 2600)
+
+(identical? 127 (dec 128))
+(identical? 128 (dec 129))
+
+(= {:a 1 :b ["hi"]}
+   (into (sorted-map) [[:b ["hi"]] [:a 1]])
+   (doto (java.util.HashMap.)
+     (.put :a 1)
+     (.put :b ["hi"])))
+(= 1 1N (Integer. 1) (Short. (short 1)) (Byte. (byte 1)))
+(= 1.25 (Float. 1.25))
+
+(= 1 1.0)
+(= 1N 1M)
+(= 1.25 5/4)
+
+(== 0.125 0.125M 1/8)
+(== 4 4N 4.0 4.0M)
+
+(defn equiv?
+  "Same as `==`, but doesn't throw an exception if any arguments are not numbers."
+  [& args]
+  (and (every? number? args)
+       (apply == args)))
+(equiv? "foo" 1)
+(equiv? 4 4N 4.0 4.0M)
+(equiv? 0.125 0.125M 1/8)
