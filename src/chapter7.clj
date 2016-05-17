eb9eaaaff9772318cd817c8968c43f9c6be63003
Jia Deng <flyucarl@gmail.com>
2016-05-18 00:05:39 +0800

    multimethod fill

--- src/chapter7.clj
+++ src/chapter7.clj
@@ -39,3 +39,53 @@
 (defmethod fill :default
   [node value]
   (assoc node [:attrs :name] (str value)))
+
+(nsmap *ns* 'fill)
+(defn- fill-dispatch [node value]
+  (if (= :input (:tag node))
+    [(:tag node) (-> node :attrs :type)]
+    (:tag node)))
+
+(defmulti fill
+  "Fill a xml/html node (as per clojure.xml)
+  with the provided value."
+  #'fill-dispatch
+  :default nil)
+
+(defmethod fill nil
+  [node value]
+  (assoc node :content [(str value)]))
+
+(defmethod fill [:input nil]
+  [node value]
+  (assoc-in node [:attrs :value] (str value)))
+
+(defmethod fill [:input "hidden"]
+  [node value]
+  (assoc-in node [:attrs :value] (str value)))
+
+(defmethod fill [:input "text"]
+  [node value]
+  (assoc-in node [:attrs :value] (str value)))
+
+(defmethod fill [:input "radio"]
+  [node value]
+  (if (= value (-> node :attrs :value))
+    (assoc-in node [:attrs :checked] "checked")
+    (update-in node [:attrs] dissoc :checked)))
+
+(defmethod fill [:input "checkbox"]
+  [node value]
+  (if (= value (-> node :attrs :value))
+    (assoc-in node [:attrs :checked] "checked")
+    (update-in node [:attrs] dissoc :checked)))
+
+(defmethod fill :default
+  [node value]
+  (assoc-in node [:attrs :name] (str value)))
+
+(fill {:tag :input
+       :attrs {:value "first choice"
+               :type "checkbox"}}
+      "first choice")
+(fill *1 "off")
