845ff2617a648b325d32fc51dbdbdaf0dbe7c596
Jia Deng <flyucarl@gmail.com>
2016-05-23 00:04:56 +0800

    multimethod

--- src/chapter7.clj
+++ src/chapter7.clj
@@ -136,3 +136,61 @@
 
 (defmethod fill :input [node value]
   (assoc-in node [:attrs :value] (str value)))
+
+(defmethod fill ::checkable [node value]
+  (if (= value (-> node :attrs :value))
+    (assoc-in node [:attrs :checked] "checked")
+    (update-in node [:attrs] dissoc :checked)))
+
+(fill {:tag :input
+       :attrs {:type "date"}}
+      "20110820")
+
+(defmethod fill nil [node value]
+  (if (= :input (:tag node))
+    (do
+      (alter-var-root #'fill-hierarchy
+                      derive (fill-dispatch node value) :input)
+      (fill node value))
+    (assoc node :content [(str value)])))
+(fill {:tag :input
+       :attrs {:type "date"}}
+      "20110820")
+
+(ns-unmap *ns* 'fill)
+
+(def input-hierarchy (-> (make-hierarchy)
+                         (derive :input.radio ::checkable)
+                         (derive :input.checkbox ::checkable)))
+
+(defn- fill-dispatch [node value]
+  (:tag node))
+
+(defmulti fill
+  "Fill a xml/html node (as per clojure.xml)
+  with the provided value."
+  #'fill-dispatch
+  :default nil)
+
+(defmulti fill-input
+  "Fill an input field."
+  (fn [node value] (-> node :attrs :type))
+  :default nil
+  :hierarchy #'input-hierarchy)
+
+(defmethod fill nil [node value]
+  (assoc node :content [(str value)]))
+
+(defmethod fill :input [node value]
+  (fill-input node value))
+
+(defmethod fill-input nil [node value]
+  (assoc-in node [:attrs :value] (str value)))
+
+(defmethod fill-input ::checkable [node value]
+  (if (= value (-> node :attrs :value))
+    (assoc-in node [:attrs :checked] "checked")
+    (update-in node [:atrrs] dissoc :checked)))
+
+(isa? fill-hierarchy [:input.checkbox :text] [::checkable :input])
+(isa? fill-hierarchy [:input.checkbox String] [::checkable CharSequence])
