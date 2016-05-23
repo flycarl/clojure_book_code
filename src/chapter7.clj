441a852114b7a621e6199dc7050e7cc9abe0ca62
Jia Deng <flyucarl@gmail.com>
2016-05-23 23:53:26 +0800

    finish chapter7 some example doesn't work as expeted, may come back later

--- src/chapter7.clj
+++ src/chapter7.clj
@@ -194,3 +194,130 @@
 
 (isa? fill-hierarchy [:input.checkbox :text] [::checkable :input])
 (isa? fill-hierarchy [:input.checkbox String] [::checkable CharSequence])
+
+(defn- fill-dispatch [node value]
+  (if-let [type (and (= :input (:tag node))
+                     (-> node :attrs :type))]
+    [(keyword (str "input." type)) (class value)]
+    [(:tag node) (class value)]))
+
+(ns-unmap *ns* 'fill)
+
+(def fill-hierarchy (-> (make-hierarchy)
+                        (derive :input.radio ::checkable)
+                        (derive :input.checkbox ::checkable)))
+
+(defn- fill-dispatch [node value]
+  (if-let [type (and (= :input (:tag node))
+                     (-> node :attrs :type))]
+    [(keyword (str "input." type)) (class value)]
+    [(:tag node) (class value)]))
+
+(defmulti fill
+  "fill a xml/html node (as per clojure.xml)
+  with the provided value."
+  #'fill-dispatch
+  :default nil
+  :hierarchy #'fill-hierarchy)
+
+(defmethod fill nil
+  [node value]
+  (if (= :input (:tag node))
+    (do
+      (alter-var-root #'fill-hierarchy
+                      derive (first (fill-dispatch node value)) :input)
+      (fill node value))
+    (assoc node :content [(str value)])))
+
+(defmethod fill
+  [:input Object] [node value]
+  (assoc-in node [:attrs :value] (str value)))
+
+(defmethod fill [::checkable clojure.lang.IPersistentSet]
+  [node value]
+  (if (contains? value (-> node ::attrs :value))
+    (assoc-in node [:attrs :checked] "checked")
+    (update-in node [:attrs] dissoc :checked)))
+
+(fill {:tag :input
+       :attrs {:value "yes"
+               :type "checkbox"}}
+      #{"yes" "y"})
+
+(fill *1 #{"no" "n"})
+
+(fill {:tag :input
+       :attrs {:type "text"}} "some text")
+
+(fill {:tag :hi} "Big Title!")
+
+(defmulti run "Executes the computation." class)
+
+(defmethod run Runnable
+  [x]
+  (.run x))
+
+(defmethod run java.util.concurrent.Callable
+  [x]
+  (.call x))
+(run #(println "hello!"))
+
+(prefer-method run java.util.concurrent.Callable Runnable)
+(run #(println "hello!"))
+
+(macroexpand-1 '(defmethod method-name dispatch-value [args] body))
+
+(defn add-method [multifn dispatch-val f]
+  (.addMethod multifn dispatch-val f))
+
+(class {})
+
+(type {})
+
+(class ^{:type :a-tag} {})
+
+(type ^{:type :a-tag} {})
+
+(ns-unmap *ns* 'run)
+
+(defmulti run "Executes the computation." type)
+
+(defmethod run Runnable
+  [x]
+  (.run x))
+
+(defmethod run java.util.concurrent.Callable
+  [x]
+  (.call x))
+
+(prefer-method run java.util.concurrent.Callable Runnable)
+
+(defmethod run :runnable-map
+  [m]
+  (run (:run m)))
+
+(run #(println "hello!"))
+(run ^{:type :runnable-map}
+  {:run #(println "hello!") :other :data})
+
+(def priorities (atom {:911-call :high
+                       :evacation :high
+                       :pothole-report :low
+                       :tree-down :low}))
+
+(defmulti route-message
+  (fn [message] (@priorities (:type message))))
+
+(defmethod route-message :low
+  [{:keys [type]}]
+  (println (format "oh, there's another %s. Put it in the log." (name type))))
+(defmethod route-message :high
+  [{:keys [type]}]
+  (println (format "Alert the authorities, thies's a %s!" (name type))))
+
+(route-message {:type :911-call})
+(route-message {:type :tree-down})
+
+(swap! priorities assoc :tree-down :high)
+
+(route-message {:type :tree-down})
